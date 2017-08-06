7281e4a8a38f1903bce1d0cb9173cacadea4f7d4
Kevin Jing Qiu <kevin@idempotent.ca>
2017-08-05 20:50:02 -0400

    Update package reference

--- phantomail.go
+++ phantomail.go
@@ -2,7 +2,7 @@ package phantomail
 
 import (
 	// plugin the directives
-	_ "github.com/kevinjqiu/phantomail/directives/hostnames"
+	_ "github.com/kevinjqiu/phantomail/smtpserver/directives/hostnames"
 	// plugin the server type
 	_ "github.com/kevinjqiu/phantomail/smtpserver"
 )
