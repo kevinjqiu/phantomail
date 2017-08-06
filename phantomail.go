d1e4f1ea4a8fd20f08d20f8eb0239ca3d834280e
Kevin Jing Qiu <kevin@idempotent.ca>
2017-08-06 01:23:37 -0400

    Rename

--- phantomail.go
+++ phantomail.go
@@ -2,7 +2,7 @@ package phantomail
 
 import (
 	// plugin the directives
-	_ "github.com/kevinjqiu/phantomail/smtpserver/directives/hostnames"
+	_ "github.com/kevinjqiu/phantomail/smtpserver/directives"
 	// plugin the server type
 	_ "github.com/kevinjqiu/phantomail/smtpserver"
 )
---
+++ smtpserver/directives/directives.go
@@ -0,0 +1,6 @@
+package directives
+
+import (
+	// Plugins
+	_ "github.com/kevinjqiu/phantomail/smtpserver/directives/hostnames"
+)
