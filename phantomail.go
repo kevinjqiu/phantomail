d0a85d16f7a08d63be41f2a92f0e8e350b861fed
Kevin Jing Qiu <kevin@idempotent.ca>
2017-08-07 11:11:10 -0400

    Add storm middleware

--- phantomail.go
+++ phantomail.go
@@ -5,6 +5,7 @@ import (
 	_ "github.com/kevinjqiu/phantomail/pkg/hostnames"
 	_ "github.com/kevinjqiu/phantomail/pkg/logreceivedmessage"
 	_ "github.com/kevinjqiu/phantomail/pkg/maildir"
+	_ "github.com/kevinjqiu/phantomail/pkg/storm"
 	// plugin the server type
 	_ "github.com/kevinjqiu/phantomail/pkg/smtpserver"
 )
--- pkg/smtpserver/plugin.go
+++ pkg/smtpserver/plugin.go
@@ -12,7 +12,7 @@ import (
 // ServerType is the server type of this Caddy plugin
 const ServerType = "smtp"
 
-var directives = []string{"hostnames", "maildir", "logreceivedmessage"}
+var directives = []string{"hostnames", "maildir", "storm", "logreceivedmessage"}
 
 func init() {
 	caddy.RegisterServerType(ServerType, caddy.ServerType{
